76d54f94d31966c9ddfa07a7d420deee6a44c1c0
Clifford Laserre <claserre9@gmail.com>
2021-02-10 06:44:51 -0800

    Authentication done - Signup and login

--- app_base/views.py
+++ app_base/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 def home(request):
     return render(request, 'app_base/home.html')
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
--- django_ajax_api/urls.py
+++ django_ajax_api/urls.py
@@ -19,13 +19,15 @@ from django.contrib import admin, auth
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from app_base.views import home
+from app_base import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
 
