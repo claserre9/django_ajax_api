6fa143f0081ed12eb68e09cbded9b08929bc59c2
Clifford Laserre <claserre9@gmail.com>
2021-02-10 08:30:16 -0800

    Auto login after sign up

--- app_base/views.py
+++ app_base/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -13,6 +14,13 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUpView, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
 
 class CreateHallView(generic.CreateView):
